4a43c5f78fc7e1bdf1b2da5cfdf1633a5eb4a93b
karmakarsubhajit <111801041@smail.iitpkd.ac.in>
2021-07-22 13:44:34 +0530

    Updating DFS solution for Path In Directed Graph

--- Graphs/PathInDirectedGraph.cpp
+++ Graphs/PathInDirectedGraph.cpp
@@ -1,4 +1,4 @@
-//BFS solution
+/* BFS solution */
 
 int Solution::solve(int A, vector<vector<int> > &B) 
 {
@@ -28,4 +28,37 @@ int Solution::solve(int A, vector<vector<int> > &B)
         }
     }
     return false;
+}
+
+
+/* DFS solution */
+
+
+bool dfs(int i, int A, vector<int>& visited, vector<vector<int>>& graph)
+{
+    if((A-1)==i)
+        return true;
+    visited[i]=1;
+    bool ans = false;
+    for(auto p:graph[i])
+    {
+        if(!visited[p])
+        {
+            ans = ans || dfs(p,A,visited,graph);
+        }
+    }
+    return ans;
+}
+
+int Solution::solve(int A, vector<vector<int> > &B) 
+{
+    vector<vector<int>> graph;
+    graph.resize(A);
+    vector<int> visited(A,0);
+    for(auto i:B)
+    {
+        graph[i[0]-1].push_back(i[1]-1);
+    }
+    return dfs(0,A, visited, graph);
+    
 }
