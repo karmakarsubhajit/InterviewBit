8672ddbd85f3e9999f4a32febefe2e80577bcf47
karmakarsubhajit <111801041@smail.iitpkd.ac.in>
2021-07-26 11:03:56 +0530

    Commiting Union Find and Union by Rank path compression

--- Graphs/CommutableIslands.cpp
+++ Graphs/CommutableIslands.cpp
@@ -128,3 +128,82 @@ int Solution::solve(int A, vector<vector<int> > &B)
     }
     return ans;
 }
+
+
+
+// Union by Rank with Path compression
+/*  Time Complexity:- O(ElogE)+O(ElogV) 
+    E can be atmost V^2 so O(logE) = O(logV)
+    Therefore the time complexity is O(ElogV)
+*/
+// Space complexity:- O(E+V)
+
+
+
+struct node
+{
+    int par;
+    int rank;
+};
+
+bool compareEdge(vector<int>&  A , vector<int>& B)
+{
+    return A[2]<B[2];
+}
+
+int findParent(vector<node>& vertices, int root)
+{
+    if(vertices[root].par!=root)
+        vertices[root].par = findParent(vertices, vertices[root].par);
+    return vertices[root].par;
+}
+
+void makeunion(vector<node>& vertices, int parA, int parB)
+{
+    if(vertices[parA].rank==vertices[parB].rank)
+    {
+        vertices[parA].par = parB;
+        vertices[parB].rank++;
+    }
+    else if(vertices[parA].rank<vertices[parB].rank)
+    {
+        vertices[parA].par = parB;
+    }
+    else if(vertices[parB].rank<vertices[parA].rank)
+    {
+        vertices[parB].par = parA;
+    }
+}
+
+int Solution::solve(int A, vector<vector<int> > &B) 
+{
+    int m = B.size();
+    int ans=0;
+    vector<node> vertices;
+    vertices.resize(A);
+    for(int i=0;i<A;i++)
+    {
+        vertices[i].par = i;  
+        vertices[i].rank =  0;
+    }
+
+
+    sort(B.begin(), B.end(), compareEdge);
+
+    for(int i=0;i<m;i++)
+    {
+        int src = B[i][0]-1;
+        int dest = B[i][1]-1;
+        int weight = B[i][2];
+        int parentA = findParent(vertices, src);
+        int parentB = findParent(vertices, dest);
+        if(parentA==parentB)
+            continue;
+        else
+        {
+            makeunion(vertices,parentA,parentB);
+            ans+=weight;
+        }
+    }
+    return ans;
+}
