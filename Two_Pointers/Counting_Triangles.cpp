12360986d55d9ed98e9c009224274c3788fe3091
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2021-08-24 08:15:05 +0530

    Create Counting_Triangles.cpp

---
+++ Two_Pointers/Counting_Triangles.cpp
@@ -0,0 +1,41 @@
+//https://www.interviewbit.com/problems/counting-triangles/
+/*
+You are given an array of N non-negative integers, A0, A1 ,…, AN-1.
+Considering each array element Ai as the edge length of some line segment, count the number of triangles which you can form using these array values.
+
+Notes:
+You can use any value only once while forming each triangle. Order of choosing the edge lengths doesn’t matter. Any triangle formed should have a positive area.
+
+Return answer modulo 109 + 7.
+For example,
+A = [1, 1, 1, 2, 2]
+Return: 4
+*/
+
+// Brute Force solution
+// Time complexity:- O(n^3)
+// Space complexity:- O(1)
+
+int Solution::nTriang(vector<int> &A) 
+{
+    int ans=0;
+    int n = A.size();
+    for(int i=0;i<n-2;i++)
+    {
+        for(int j=i+1;j<n-1;j++)
+        {
+            for(int k=j+1;k<n;k++)
+            {
+                if((i!=j)&&(j!=k))
+                {
+                    int a = A[i];
+                    int b = A[j];
+                    int c = A[k];
+                    if(a+b>c && b+c>a && c+a>b)
+                        ans++;
+                }
+            }
+        }
+    }
+    return ans;
+}
