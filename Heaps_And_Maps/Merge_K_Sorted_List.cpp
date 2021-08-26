bd35d4cd37bd195448b040745c00a1a2f55233d5
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2021-08-26 16:10:30 +0530

    Update Merge_K_Sorted_List.cpp

--- Heaps_And_Maps/Merge_K_Sorted_List.cpp
+++ Heaps_And_Maps/Merge_K_Sorted_List.cpp
@@ -12,6 +12,62 @@ will result in
 1 -> 3 -> 4 -> 8 -> 9 -> 10 -> 11 -> 13 -> 20
 */
 
+
+// Using merge sorted lists approach
+// Time complexity:- O(nk^2), where k is no of lists, and n is no of nodes in a list
+// Space complexity:- O(1)
+
+ListNode* mergeTwoList(ListNode* a, ListNode* b)
+{
+    if(a==NULL)
+        return b;
+    if(b==NULL)
+        return a;
+    ListNode* dummy = new ListNode(-1);
+    ListNode* temp = dummy;
+    while(a && b)
+    {
+        if(a->val<b->val)
+        {
+            temp->next = a;
+            a = a->next;
+            temp = temp->next;
+        }
+        else
+        {
+            temp->next = b;
+            b = b->next;
+            temp = temp->next;
+        }
+    }
+    while(a)
+    {
+         temp->next = a;
+        a = a->next;
+        temp = temp->next;
+    }
+    while(b)
+    {
+         temp->next = b;
+        b = b->next;
+        temp = temp->next;
+    }
+    return dummy->next;
+}
+
+
+ListNode* Solution::mergeKLists(vector<ListNode*> &lists) 
+{
+     if(lists.size()==0)
+        return NULL;
+    ListNode* head=lists[0];
+    for(int i=1;i<lists.size();i++)
+    {
+        head= mergeTwoList(head,lists[i]);
+    }
+    return head;
+}
+
 // Using min heap approach
 // Time complexity:- O(nklogk), where k is no of lists, and n is no of nodes in a list
 // Space complexity:- O(k)
