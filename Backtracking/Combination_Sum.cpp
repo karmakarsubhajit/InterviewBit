038ab65e56b8a5f11630776363e448bb68866e81
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2021-08-31 08:55:13 +0530

    Create Combination_Sum.cpp

---
+++ Backtracking/Combination_Sum.cpp
@@ -0,0 +1,79 @@
+// https://www.interviewbit.com/problems/combination-sum/
+
+/*
+Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+
+All numbers (including target) will be positive integers.
+Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+The combinations themselves must be sorted in ascending order.
+CombinationA > CombinationB iff (a1 > b1) OR (a1 = b1 AND a2 > b2) OR … (a1 = b1 AND a2 = b2 AND … ai = bi AND ai+1 > bi+1)
+The solution set must not contain duplicate combinations.
+Example, 
+
+Given candidate set 2,3,6,7 and target 7,
+
+A solution set is:
+
+[2, 2, 3]
+[7]
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Time complexity:- O(n^n)
+// Space complexity:- O(n)
+
+    void getComb(vector<vector<int>>& ans, vector<int>& v, vector<int> candidates, int n, int i, int sm, int target)
+    {
+        if(sm>target)
+            return;
+        if(sm==target)
+        {
+            ans.push_back(v);   
+            return;
+        }
+        if(i>=n)
+            return;
+        for(int p=i;p<n;p++)
+        {  
+            while((p+1<n) && (candidates[p]==candidates[p+1]))
+            {
+                p++;
+            }
+            v.push_back(candidates[p]);
+            getComb(ans, v, candidates,n,p,sm+candidates[p],target);
+            v.pop_back();
+        }
+    }
+
+
+vector<vector<int> > Solution::combinationSum(vector<int> &A, int target) 
+{
+    int n = A.size();    
+    sort(A.begin(), A.end());
+    vector<vector<int>> ans;
+    vector<int> v;
+    int sm = 0;
+    getComb(ans, v, A, n, 0, sm, target);
+    sort(ans.begin(), ans.end());
+    return ans;
+}
+
