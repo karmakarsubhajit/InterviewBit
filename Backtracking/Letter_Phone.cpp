574e85f86aa2f840d9e01f88a70fbe0e2085f357
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2021-09-03 08:25:10 +0530

    Update Letter_Phone.cpp

--- Backtracking/Letter_Phone.cpp
+++ Backtracking/Letter_Phone.cpp
@@ -14,7 +14,6 @@ Make sure the returned strings are lexicographically sorted.
 
 */
 
-
 // Recursive soln
 // Time complexity:- (n*3^n)
 // Space complexity:- O(n), stack space
@@ -56,3 +55,44 @@ vector<string> Solution::letterCombinations(string A)
         getAns(temp, ans, 0, A,A.size(),mp);
         return ans;
 }
+
+
+
+// Iterative soln
+// Time complexity:- (n*3^n)
+// Space complexity:- O(1)
+
+
+vector<string> Solution::letterCombinations(string A) 
+{
+        if(A.length()==0)
+          return {};
+        map<char,vector<char>> mp;
+        mp['0']={'0'};
+        mp['1']={'1'};
+        mp['2']={'a','b','c'};
+        mp['3']={'d','e','f'};
+        mp['4']={'g','h','i'};
+        mp['5']={'j','k','l'};
+        mp['6']={'m','n','o'};
+        mp['7']={'p','q','r','s'};
+        mp['8']={'t','u','v'};
+        mp['9']={'w','x','y','z'};
+        vector<string> ans;
+        ans.push_back("");
+        for(auto i:A)
+        {
+            vector<string> temp;
+            for(auto ch:mp[i])
+            {
+                for(auto item:ans)
+                {
+                    temp.push_back(item+ch);
+                }
+            }
+            ans.swap(temp);
+        }
+       
+        return ans;
+}
+
