bf59db53a711319e72a9fffc9c1b8178cf973da8
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2021-09-06 11:44:08 +0530

    Commit Iterative soln

--- Trees/Max_Depth.cpp
+++ Trees/Max_Depth.cpp
@@ -15,6 +15,7 @@ max depth = 2.
 
 */
 
+// Recursive soln
 // Time complexity:- O(n)
 // Space complexity:- O(n), stack space
 
@@ -26,3 +27,36 @@ int Solution::maxDepth(TreeNode* A)
        return 1;
     return 1+max(maxDepth(A->left),maxDepth(A->right));
 }
+
+
+
+// Iterative soln
+// Time complexity:- O(n)
+// Space complexity:- O(n/2) = O(n)
+
+
+int Solution::maxDepth(TreeNode* A) 
+{
+   if(A==NULL)
+      return 0;
+    queue<TreeNode*> q;
+    q.push(A);
+    int depth = 0;
+    while(q.size()!=0)
+    {
+        int n = q.size();
+        for(int i=1;i<=n;i++)
+        {
+            TreeNode* temp = q.front();
+            q.pop();
+            if(temp->left)
+                q.push(temp->left);
+            if(temp->right)
+                q.push(temp->right);
+        }
+        depth++;
+    }
+    return depth;
+    
+}
+
