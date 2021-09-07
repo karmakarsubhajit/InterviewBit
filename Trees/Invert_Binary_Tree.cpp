0e4f119ffb84d659352e3b1524afca1d7c6b84ed
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2021-09-07 16:49:45 +0530

    Create Invert_Binary_Tree.cpp

---
+++ Trees/Invert_Binary_Tree.cpp
@@ -0,0 +1,89 @@
+// https://www.interviewbit.com/problems/invert-the-binary-tree/
+/*
+Problem Description
+
+Given a binary tree A, invert the binary tree and return it. 
+
+ Inverting refers to making left child as the right child and vice versa.
+
+
+
+Problem Constraints
+1 <= size of tree <= 100000
+
+
+
+Input Format
+First and only argument is the head of the tree A.
+
+
+
+Output Format
+Return the head of the inverted tree.
+
+
+
+Example Input
+Input 1:
+
+ 
+     1
+   /   \
+  2     3
+Input 2:
+
+ 
+     1
+   /   \
+  2     3
+ / \   / \
+4   5 6   7
+
+
+Example Output
+Output 1:
+
+ 
+     1
+   /   \
+  3     2
+Output 2:
+
+ 
+     1
+   /   \
+  3     2
+ / \   / \
+7   6 5   4
+
+
+Example Explanation
+Explanation 1:
+
+Tree has been inverted.
+Explanation 2:
+
+Tree has been inverted.
+*/
+
+// Time complexity:- O(n)
+// Space complexity:- O(height)
+
+TreeNode* invertedRoot(TreeNode* A)
+{
+    if(A==NULL)
+        return A;
+    TreeNode* lft = invertedRoot(A->left);
+    TreeNode* rgt = invertedRoot(A->right);
+    TreeNode* temp = lft;
+    lft = rgt;
+    rgt=temp;
+    A->left = lft;
+    A->right = rgt;
+    return A;
+}
+
+TreeNode* Solution::invertTree(TreeNode* A) 
+{
+    return invertedRoot(A);
+}
