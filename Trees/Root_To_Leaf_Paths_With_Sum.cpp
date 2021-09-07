7cb2d990068570affbcf2a3d5fdca77fee836eb6
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2021-09-07 18:59:37 +0530

    Update Root_To_Leaf_Paths_With_Sum.cpp

--- Trees/Root_To_Leaf_Paths_With_Sum.cpp
+++ Trees/Root_To_Leaf_Paths_With_Sum.cpp
@@ -1,7 +1,7 @@
 // https://www.interviewbit.com/problems/root-to-leaf-paths-with-sum/
 
 
-// Recursive soln
+// Recursive soln || DFS
 // Time complexity:- O(h*n/2) = O(nh)
 // Space complexity:- O(height of tree)
 
@@ -41,3 +41,66 @@ vector<vector<int> > Solution::pathSum(TreeNode* A, int B)
     dfs(A, B, 0 , ans, p);
     return ans;
 }
+
+
+
+// Iterative soln || BFS || Level Order
+// Time complexity:- O(h*n/2) = O(nh)
+// Space complexity:- O(h*n/2) = O(nh)
+
+
+struct node{
+    TreeNode* n;
+    int sm;
+    vector<int> v;
+};
+
+vector<vector<int> > Solution::pathSum(TreeNode* A, int B) 
+{
+    vector<vector<int>> ans;
+    vector<int> p;
+    queue<node> q;
+    node i;
+    i.n = A;
+    i.sm = 0;
+    i.v = p;
+    q.push(i);
+    while(q.size()!=0)
+    {
+        int sz = q.size();
+        for(int c=1;c<=sz;c++)
+        {
+            node curr = q.front();
+            q.pop();
+            TreeNode* n = curr.n;
+            int sm = curr.sm;
+            vector<int> v = curr.v;
+            sm+=n->val;
+            v.push_back(n->val);
+            if((sm==B)&& (n->left==NULL) && (n->right==NULL))
+            {
+                ans.push_back(v);
+            }
+            if(n->left)
+            {
+                node tm;
+                tm.n = n->left;
+                tm.sm = sm;
+                tm.v = v;
+                q.push(tm);
+            }
+            if(n->right)
+            {
+                node tm;
+                tm.n = n->right;
+                tm.sm = sm;
+                tm.v = v;
+                q.push(tm);
+            }
+        }
+    }
+
+  
+    return ans;
+}
+
