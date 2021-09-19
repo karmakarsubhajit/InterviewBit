aeadf3c402b1d81b98f25404ac8209c79c941e65
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2021-09-19 07:55:58 +0530

    Update Array_3_pointers.cpp

--- Two_Pointers/Array_3_pointers.cpp
+++ Two_Pointers/Array_3_pointers.cpp
@@ -41,3 +41,35 @@ int Solution::minimize(const vector<int> &A, const vector<int> &B, const vector<
     }
     return ans;
 }
+
+
+// Optimal solution || Linear
+// Time complexity:- O(n1+n2+n3), where n1, n2 and n3 are size of array
+// Space complexity:- O(1)
+
+
+int Solution::minimize(const vector<int> &A, const vector<int> &B, const vector<int> &C) 
+{
+    int ans =INT_MAX;
+    int n1 = A.size();
+    int n2 = B.size();
+    int n3 = C.size();
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    while((i<n1) && (j<n2) && (k<n3))
+    {
+        int mn = min(min(A[i],B[j]),C[k]);
+        int mx = max(max(A[i],B[j]),C[k]);
+        ans = min(ans,mx-mn);
+        if(ans==0)
+           return ans;
+        if(mn==A[i])
+            i++;
+        else if(mn==B[j])
+            j++;
+        else 
+            k++;
+    }
+    return ans;
+}
