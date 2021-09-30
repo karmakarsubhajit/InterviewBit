e3d180313edb3c9cdcbf03c0ecdd2e5c5a5e74dd
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2021-09-30 09:43:51 +0530

    Update First_Missing_Integer.cpp

--- Arrays/First_Missing_Integer.cpp
+++ Arrays/First_Missing_Integer.cpp
@@ -12,6 +12,33 @@ Given [1,2,0] return 3,
 
 */
 
+
+
+
+// Sorting based soln || 1 pass solution
+// Time Complexity:- O(nlogn)
+// Space Complexity:- O(1)
+
+int Solution::firstMissingPositive(vector<int> &A) 
+{
+    sort(A.begin(),A.end());
+    int c=1;
+    for(int i=0;i<A.size();i++)
+    {
+        if(A[i]<=0)
+            continue;
+        else if(A[i]==c)
+        {
+            c++;
+        }    
+        else
+            return c;
+    }
+
+    return c;
+}
+
+
 // Hashing Solution || 2 pass solution
 // Time Complexity:- O(n)
 // Space Complexity:- O(n)
