7eba271c44281d7a67530e0798912b27716165f8
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2021-09-30 10:53:59 +0530

    Update First_Missing_Integer.cpp

--- Arrays/First_Missing_Integer.cpp
+++ Arrays/First_Missing_Integer.cpp
@@ -84,3 +84,34 @@ int Solution::firstMissingPositive(vector<int> &A)
     }
     return A.size()+1;
 }
+
+// Constant Space || 3 pass solution
+// Time Complexity:- O(n)
+// Space Complexity:- O(1)
+
+
+int Solution::firstMissingPositive(vector<int> &nums) 
+{
+    bool flag=false;
+        int n = nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==1)
+                flag=true;
+            if(nums[i]<=0 || nums[i]>n)
+                nums[i]=1;
+        }
+        if(flag==false)
+            return 1;
+        for(int i=0;i<n;i++)
+        {
+            int index = abs(nums[i]);
+            nums[index-1]=-1*abs(nums[index-1]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>0)
+                return i+1;
+        }
+        return n+1;
+}
