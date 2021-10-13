f4a888efad41c9fb49c219d55a5378b0ed268f65
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2021-10-13 15:57:56 +0530

    Update Min_Sum_Path_in_Triangle.cpp

--- Dynamic_Programming/Min_Sum_Path_in_Triangle.cpp
+++ Dynamic_Programming/Min_Sum_Path_in_Triangle.cpp
@@ -18,6 +18,29 @@ The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
 
 
 
+// Bottom Up || Optimal soln || Modifying matrix
+// Time Complexity:- O(m*n), m,n are no of rows and cols in triangle
+// Space Complexity:- O(1)
+
+
+
+int Solution::minimumTotal(vector<vector<int> > &A) {
+  
+  
+    int m = A.size();
+    for(int i=m-2;i>=0;i--)
+    {
+        int n = A[i].size();
+        for(int j=n-1;j>=0;j--)
+        {
+            A[i][j]+=min(A[i+1][j],A[i+1][j+1]);
+        }
+    }
+    return A[0][0];
+}
+
+
+
 
 // Bottom Up || Optimal soln
 // Time Complexity:- O(m*n), m,n are no of rows and cols in triangle
