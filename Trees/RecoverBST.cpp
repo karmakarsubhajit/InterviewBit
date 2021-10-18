b51dfac0f5d30d63c0c314bb18e097d49f25f920
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2021-10-18 17:01:40 +0530

    Create RecoverBST.cpp

---
+++ Trees/RecoverBST.cpp
@@ -0,0 +1,78 @@
+// https://www.interviewbit.com/problems/recover-binary-search-tree/
+/*
+Two elements of a binary search tree (BST) are swapped by mistake.
+
+Tell us the 2 values swapping which the tree will be restored.
+
+Note:
+
+A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
+
+Example :
+
+
+Input : 
+         1
+        / \
+       2   3
+
+Output : 
+       [1, 2]
+
+Explanation : Swapping 1 and 2 will change the BST to be 
+         2
+        / \
+       1   3
+which is a valid BST          
+*/
+
+// Extra space by vector and recursion stack
+// Time Complexity:- O(n)
+// Space Complexity:- O(n)
+
+
+void inorder(TreeNode* r, vector<int>& t)
+{
+    if(r==NULL)
+        return;
+    inorder(r->left,t);
+    t.push_back(r->val);
+    inorder(r->right,t);
+}
+
+
+vector<int> Solution::recoverTree(TreeNode* A) 
+{
+    vector<int> t;
+    inorder(A,t);
+    int a=INT_MIN,b=INT_MIN,c=INT_MIN;
+    for(int i=1;i<t.size();i++)
+    {
+        if(t[i]<t[i-1])
+        {
+            if(a==INT_MIN)
+            {
+                a = t[i-1];
+                b = t[i];
+            }
+            else
+            {
+                c=t[i];
+            }
+        }
+    }
+    vector<int> ans;
+    if(c==INT_MIN)
+    {
+        ans.push_back(b);
+        ans.push_back(a);
+        
+    }
+    else
+    {
+        ans.push_back(c);
+        ans.push_back(a);
+        
+    }
+    return ans;
+}
