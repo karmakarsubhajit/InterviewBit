b90bb1550a3a0672b9cd5a037ad85329c1809767
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2021-10-25 07:44:40 +0530

    Create MergeOverlappingInterval.cpp

---
+++ Arrays/MergeOverlappingInterval.cpp
@@ -0,0 +1,63 @@
+// https://www.interviewbit.com/problems/merge-overlapping-intervals/
+
+/*
+Given a collection of intervals, merge all overlapping intervals.
+
+For example:
+
+Given [1,3],[2,6],[8,10],[15,18],
+
+return [1,6],[8,10],[15,18].
+
+Make sure the returned intervals are sorted.
+
+*/
+
+
+// Time complexity:- O(nlogn)
+// Space complexity:- O(n)
+
+bool compare(Interval& a, Interval& b)
+{
+    return a.start<b.start;
+}
+
+vector<Interval> Solution::merge(vector<Interval> &A) 
+{
+    sort(A.begin(), A.end(),compare);
+    vector<Interval> ans;
+    int start = -1;
+    int end = -1;
+    for(auto it:A)
+    {
+        if(start==-1)
+        {
+            start = it.start;
+            end = it.end;
+        }
+        else
+        {
+            if(end<it.start)
+            {
+                Interval temp;
+                temp.start = start;
+                temp.end = end;
+                ans.push_back(temp);
+                start = it.start;
+                end = it.end;
+            }
+            else
+            {
+                end = max(end, it.end);
+            }
+        }
+
+    }
+    Interval temp;
+    temp.start = start;
+    temp.end = end;
+    ans.push_back(temp);
+
+    return ans;
+
+}
