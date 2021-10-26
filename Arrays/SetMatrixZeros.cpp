2bf4b135e9d74e2e7f01f8699b231a9d8159a2de
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2021-10-26 21:37:40 +0530

    Update SetMatrixZeros.cpp

--- Arrays/SetMatrixZeros.cpp
+++ Arrays/SetMatrixZeros.cpp
@@ -119,3 +119,63 @@ void Solution::setZeroes(vector<vector<int> > &matrix) {
         }
 
 }
+
+
+
+// Constant space || Optimal 
+// Time complexity:- O(m*n)
+// Space complexity:- O(1)
+
+void Solution::setZeroes(vector<vector<int> > &matrix) 
+{
+       int m = matrix.size();
+        int n = matrix[0].size();
+        
+        
+        bool isCol = false;
+        
+        
+        for(int i=0;i<m;i++)
+        {
+            if(matrix[i][0]==0)
+                isCol = true;
+            
+            for(int j=1;j<n;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                if((matrix[i][0]==0) || (matrix[0][j]==0))
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(matrix[0][0]==0)
+        {
+            for(int j=0;j<n;j++)
+            {
+               matrix[0][j]=0;
+            }
+        }
+        
+        if(isCol==1)
+        {
+            for(int i=0;i<m;i++)
+            {
+               matrix[i][0]=0;
+            }
+        }
+        
+        
+
+}
