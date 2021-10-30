ff9a25a40f4b0a975af15aaca4f8d62cf2f41424
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2021-10-30 15:23:56 +0530

    Update Nby3RepeatNumber.cpp

--- Arrays/Nby3RepeatNumber.cpp
+++ Arrays/Nby3RepeatNumber.cpp
@@ -67,3 +67,61 @@ int Solution::repeatedNumber( vector<int> A)
     }
     return -1;
 }
+
+
+
+// Moore's Voting Algorithm | Optimal soln
+// Time complexity:- O(n)
+// Space complexity:- O(1)
+
+
+int Solution::repeatedNumber( vector<int> A)
+ {
+     
+    int count1 =0;
+    int count2=0;
+    int num1 = INT_MAX;
+    int num2 = INT_MAX;
+
+    for(int i=0;i<A.size();i++)
+    {
+        if(num1==A[i])
+        {
+            count1++;
+        }
+        else if(num2==A[i])
+        {
+            count2++;
+        }
+        else if(count1==0)
+        {
+            num1 = A[i];
+            count1++;
+        }
+        else if(count2==0)
+        {
+            num2 = A[i];
+            count2++;
+        }
+        else
+        {
+            count1--;
+            count2--;
+        }
+    }
+    count1=0;
+    count2=0;
+    for(int i=0;i<A.size();i++)
+    {
+        if(A[i]==num1)
+            count1++;
+        else if(A[i]==num2)
+            count2++;
+    }
+    if(count1>A.size()/3)
+        return num1;
+    if(count2>A.size()/3)
+        return num2;
+    return -1;
+    
+}
